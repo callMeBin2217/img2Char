e6dd21268730b4b350d0aa1ba2651f6a06c74233
HuBin <343603146@qq.com>
2018-02-09 15:43:43 +0800

    update

--- img2Char.py
+++ img2Char.py
@@ -10,29 +10,32 @@ import argparse
 
 class img2Char():
 	def __init__(self):
-		self.IMG = r'C:/Users/LENOVO/Desktop/img1/b.jpg'
+		self.IMG = r'C:/Users/LENOVO/Desktop/img1/569.jpg'
 		self.WIDTH =100
 		self.HEIGHT =50
 
-
-		self.ascii_char = list(r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!;:,\"^`.")
+		#@#&$*ox!i;.
+		#$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!;:,\"^`.
+		self.ascii_char = list(r"@MNGQ&OCo0>!:-;.")
 
 
 	def get_char(self,r,g,b,alpha=256):
 		if alpha==0:
 			return ' '
 		length = len(self.ascii_char)
-		gray = int(0.2126*r+0.7152*g+0.0722*b)
+		#gray = (0.299*r+0.587*g+0.114*b)
+		#gray = (299*r+587*g+114*b+500)/1000
+		gray = (r**2.2*0.2973+g**2.2*0.6274+b**2.2*0.0753)**(1/2.2)
 		unit = (256.0+1)/length
 		return self.ascii_char[int(gray/unit)]
 
 
 	def save2File(self):
 		im = Image.open(self.IMG)
-		#scale = max(im.size[0]/self.WIDTH,im.size[1]/self.HEIGHT)
-		#self.WIDTH = im.size[0]/scale *2
-		#self.HEIGHT = im.size[1]/scale
-		im = im.resize((self.WIDTH,self.HEIGHT),Image.NEAREST)
+		scale = max(im.size[0]/self.WIDTH,im.size[1]/self.HEIGHT)
+		self.WIDTH =int(im.size[0]/scale *2)
+		self.HEIGHT = int(im.size[1]/scale)
+		im = im.resize((self.WIDTH,self.HEIGHT),Image.ANTIALIAS)
 		txt = ""
 
 		for i in range(self.HEIGHT):
